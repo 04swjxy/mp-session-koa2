502af9c011f99fca22cb6ea95c24d9a32ca1d95b
04swjxy <04swjxy@163.com>
2019-01-20 17:10:28 +0800

    v0.1.4 modify+

--- test/server/config.js
+++ test/server/config.js
@@ -4,8 +4,8 @@ const { RedisStore } = require('../../index');
 const config = {
   port: 8080,
   sessionOption: {
-    appId: 'wxf1dc939a9ea2b673',
-    appSecret: '8f4d7702d2c32e727aa17330222a77c5',
+    appId: '',  //请填写自己的appId
+    appSecret: '',  //请填写自己的appSecret
     loginPath: '/api/login',
     grant_type: 'authorization_code',
     maxAge: 3600,  //session 自动过期时间,单位:秒
